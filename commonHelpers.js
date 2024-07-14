9c0c9e02399ea8168f2031f6113365ecebd584d2
ruslana-semkiv18 <156903697+ruslana-semkiv18@users.noreply.github.com>
2024-07-14 15:58:46 +0000

    Deploying to gh-pages from @ ruslana-semkiv18/goit-js-hw-11@b0c5097e8aa5e6fae9186e3f25c197805595730b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,14 @@
-import{i as a}from"./assets/vendor-8e8cd629.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const l="44916232-ab67e58f2cd7c4a8ed03790be",d="https://pixabay.com/api/",u=document.querySelector(".card-container"),f=document.querySelector(".search-form");f.addEventListener("submit",p);function m(o){return fetch(`${d}?key=${l}&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}function p(o){o.preventDefault();const t=o.currentTarget,n=t.elements.query.value.toLowerCase();m(n).then(s=>h(s.hits)).catch(y).finally(()=>t.reset())}function h(o){u.innerHTML=o.map(({webformatURL:t,largeImageURL:n,tags:s,likes:e,views:r,comments:i,downloads:c})=>`<div class="card">
+import{S as u,i as f}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const m="44916232-ab67e58f2cd7c4a8ed03790be",p="https://pixabay.com/api/",a=document.querySelector(".card-container"),h=document.querySelector(".search-form");h.addEventListener("submit",g);function y(o){return v(),fetch(`${p}?key=${m}&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).finally(()=>{w()})}function g(o){o.preventDefault();const t=o.currentTarget,i=t.elements.query.value.trim().toLowerCase();i&&(a.innerHTML="",y(i).then(s=>{if(s.hits.length===0)throw new Error;b(s.hits)}).catch(L).finally(()=>t.reset()))}function b(o){a.innerHTML=o.map(({webformatURL:i,largeImageURL:s,tags:e,likes:r,views:n,comments:l,downloads:d})=>`<div class="card">
         <div class="card-img">
-         <a href="${n}"><img src="${t}" alt="${s}"></a>
+         <a href="${s}"><img src="${i}" alt="${e}"></a>
         </div>
         <div class="card-body">
          <ul class="card-list">
-          <li class="card-item"><span>Likes</span>${e}</li>
-          <li class="card-item"><span>Views</span>${r}</li>
-          <li class="card-item"><span>Comments</span>${i}</li>
-          <li class="card-item"><span>Downloads</span>${c}</li>
+          <li class="card-item"><span>Likes</span>${r}</li>
+          <li class="card-item"><span>Views</span>${n}</li>
+          <li class="card-item"><span>Comments</span>${l}</li>
+          <li class="card-item"><span>Downloads</span>${d}</li>
          </ul>
        </div>
-      </div>`).join("")}function y(o){a.error({title:"Error",message:"Illegal operation"})}
+      </div>`).join(""),new u(".card-img a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250}).refresh()}function L(o){f.error({message:"Sorry, there are no images matching your search query.Please try again!",messageColor:"#fff",messageSize:"16px",messageLineHeight:"1.5",messageFontWeight:"400",backgroundColor:"#ef4040",close:!0,position:"topRight",progressBarColor:"#b51b1b"})}const c=document.querySelector(".loader");function v(){c.style.display="block"}function w(){c.style.display="none"}
 //# sourceMappingURL=commonHelpers.js.map
