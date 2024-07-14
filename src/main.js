4938595034b57561f43b404fcfeba71ca2f23062
ruslana-semkiv18 <semkivruslana18@gmail.com>
2024-07-14 17:31:13 +0300

    update css

--- src/main.js
+++ src/main.js
@@ -1,68 +1,63 @@
-
 import iziToast from "izitoast";
-
 import "izitoast/dist/css/iziToast.min.css";
-
 import SimpleLightbox from "simplelightbox";
-
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-
-// key — твій унікальний ключ доступу до API.
-// q — слово для пошуку. Те, що буде вводити користувач.
-// image_type photo.
-// orientation horizontal.
-// safesearch  true.
-
-function fetchGallery(q) {
-  return fetch(`https://pixabay.com/api/?key=44916232-ab67e58f2cd7c4a8ed03790be&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`).then((res) => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
-}
+const key = "44916232-ab67e58f2cd7c4a8ed03790be";
+const url = "https://pixabay.com/api/";
 
 const containerCard = document.querySelector(".card-container");
-
 const form = document.querySelector(".search-form");
+
 form.addEventListener("submit", handlerSearch);
+
+
+function fetchGallery(q) {
+  return fetch(`${url}?key=${key}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    });
+}
+
+
+
 function handlerSearch(evt) {
   evt.preventDefault();
 
-  const form = evt.currentTarget; // посилання на елемент форми
-  const queryValue = form.elements.query.value.toLowerCase(); // значення, яке написав користувач
+  const form = evt.currentTarget;
+  const queryValue = form.elements.query.value.toLowerCase(); 
 
-  fetchGallery(queryValue) // робимо запит на сервер та отримуємо відповідь
-    .then(renderCard) // запускаємо функцію, яка відмалює наші карточки
-    .catch(onFetchError) // якщо робиться запит на неіснуючого покемона (404) - викликається ця фукнція для обробки помилки
-    .finally(() => form.reset()); // після закінчення промісу ми очищуємо дані форми
+  fetchGallery(queryValue) 
+    .then(data=>renderCard(data.hits)) 
+    .catch(onFetchError) 
+    .finally(() => form.reset()); 
 }
 
 
 
 function renderCard(img) {
-    containerCard.innerHTML = img.map(({ webformatUrl, largeImageURL, tags, likes, views, comments, downloads })=>
-        `<div class="card">
-   <div class="card-img">
-     <a href="${largeImageURL}"><img src="${webformatUrl}" alt="${tags}"></a>
-   </div>
-   <div class="card-body">
-     <ul class="list-group">
-       <li><span>Likes</span>${likes}</li>
-       <li><span>Views</span>${views}</li>
-       <li><span>Comments</span>${comments}</li>
-       <li><span>Downloads</span>${downloads}</li>
-     </ul>
-   </div>
-</div>`).join("");
+  containerCard.innerHTML = img.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+      `<div class="card">
+        <div class="card-img">
+         <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}"></a>
+        </div>
+        <div class="card-body">
+         <ul class="card-list">
+          <li class="card-item"><span>Likes</span>${likes}</li>
+          <li class="card-item"><span>Views</span>${views}</li>
+          <li class="card-item"><span>Comments</span>${comments}</li>
+          <li class="card-item"><span>Downloads</span>${downloads}</li>
+         </ul>
+       </div>
+      </div>`).join("");
 }
 
 function onFetchError(error) {
-
-      iziToast.error({
-        title: 'Error',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-      });
+  iziToast.error({
+    title: 'Error',
+    message: 'Illegal operation',
+});
 }
